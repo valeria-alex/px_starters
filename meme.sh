b2d7cae80fa0f8e656f3f5e7b5a00a681a808449
valeria-alex <47971956+valeria-alex@users.noreply.github.com>
2021-02-20 18:23:28 +0100

    Update meme.sh
    removed old ROS source

--- meme.sh
+++ meme.sh
@@ -4,7 +4,6 @@
 export PX4_HOME_LAT=55.057883   
 export PX4_HOME_LON=10.569678
 
-source /opt/ros/kinetic/setup.bash
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
